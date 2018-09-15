6ea1703e34420a0f42b1aa9a31a48ed19c3a5c65
SHOUN <shooouuun@gmail.com>
2018-09-16 02:43:13 +0300

    Фикс Д17. Добавлена анимация ошибки

--- js/scripts.js
+++ js/scripts.js
@@ -1,14 +1,24 @@
 var link = document.querySelector(".search-hotel__btn");
-var form = document.querySelector("form");
+var form = document.querySelector(".form");
+var popup = document.querySelector(".popup__search");
+var date = popup.querySelector(".search-hotel__input");
+var arrivalDate = popup.querySelector("[name = hotel-date-in-field]");
+var departureDate = popup.querySelector("[name = hotel-date-out-field]");
+
 link.addEventListener('click', showForm);
 
 form.addEventListener("submit", function (evt) {
   evt.preventDefault();
 })
 
+form.addEventListener("submit", function (evt) {
+  if (!arrivalDate.value || !dapertireDate.value) {
+    evt.preventDefault();
+    popup.classList.add("modal-error");
+  }
+});
+
 function showForm(e) {
-  var popup = document.querySelector('.popup__search');
-  var date = popup.querySelector(".search-hotel__input");
   e.preventDefault();
 
   if (popup.classList.contains('open')) {
